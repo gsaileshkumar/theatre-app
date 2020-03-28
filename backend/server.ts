f8d37b4308e1657de8276a676225a86d0b6fcbd0
Sailesh Kumar <sailesh.kumar@chordx.co>
2020-03-28 15:33:57 +0800

    cookie samesite none

--- backend/server.ts
+++ backend/server.ts
@@ -18,7 +18,7 @@ app.use(
   })
 );
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(session({ secret: "ohmysecret" }));
+app.use(session({ secret: "ohmysecret", cookie: { sameSite: "none" } }));
 
 app.use("/auth", auth);
 
