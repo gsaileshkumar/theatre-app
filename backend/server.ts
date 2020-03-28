e4a2cc1b46d251d57425e49a7fc5c00b01869291
Sailesh Kumar <sailesh.kumar@chordx.co>
2020-03-28 15:02:18 +0800

    cors withcredentials added

--- backend/server.ts
+++ backend/server.ts
@@ -11,7 +11,11 @@ import { isValidUserMiddleware } from "./middleware/authorization";
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(
+  cors({
+    credentials: true
+  })
+);
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(session({ secret: "ohmysecret" }));
 
