a94d8f4a2b5a83ecffe899944b9abc2ed10a541b
Sailesh Kumar <gsaileshkumar@gmail.com>
2021-07-03 00:19:39 +0530

    updated cors

--- backend/server.ts
+++ backend/server.ts
@@ -14,7 +14,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  origin: 'https://binge-watch.netlify.app'
+}));
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.use(
